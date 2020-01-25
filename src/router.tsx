398d1c4ff4e190df255981728a527f1cb0669dc9
Jadilson Guedes <jadilson12@gmail.com>
2020-01-25 16:58:18 -0300

    perf: mexendo com a router

---
+++ src/router.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import { CategoriaComponent } from './modules/categoria/categoria-component';
+import { HomeComponent } from './modules/home/home-component';
+import { ProdutoComponent } from './modules/produto/produto-component';
+
+import HomeIcon from '@material-ui/icons/Home';
+import CategoryIcon from '@material-ui/icons/Category';
+
+const routes = [
+  {
+    icon: <HomeIcon />,
+    name: 'Home',
+    path: '/',
+    exact: true,
+    main: <HomeComponent />,
+  },
+  {
+    icon: <CategoryIcon />,
+    name: 'Categorias',
+    path: '/categoria',
+    main: <CategoriaComponent />,
+  },
+  {
+    icon: <CategoryIcon />,
+    name: 'Produtos',
+    path: '/produto',
+    main: <ProdutoComponent />,
+  },
+];
+
+export const AppRoutes = routes;
+
+export function SidebarExample() {
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <div
+          style={{
+            padding: '10px',
+            width: '40%',
+            background: '#f0f0f0',
+          }}
+        >
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/categoria">categoria</Link>
+            </li>
+            <li>
+              <Link to="/produto">produto</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div style={{ flex: 1, padding: '10px' }}>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route key={index} path={route.path} exact={route.exact} children={<route.main />} />
+            ))}
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
